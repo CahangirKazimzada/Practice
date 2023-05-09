b0988b90a37b4ce2eff51abccde19d9a85da91d2
kubra <7chpkku@code.edu.az>
2023-05-09 17:23:12 +0400

    Agency task

---
+++ app.js
@@ -0,0 +1,6 @@
+// const icon = document.querySelector("i");
+// const respList = document.querySelector(".resp-list");
+
+// icon.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "red";
+// })
